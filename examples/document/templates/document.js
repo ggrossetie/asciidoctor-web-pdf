f54776198927549cbcbb7265ca89c08df7458ab1
Guillaume Grossetie <ggrossetie@gmail.com>
2018-05-17 19:10:19 +0200

    (wip) Create a stylesheet and a converter for a PDF document

---
+++ examples/document/templates/document.js
@@ -0,0 +1,16 @@
+module.exports = (ctx) => `<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<link href="./asciidoctor.css" rel="stylesheet">
+<link href="./document.css" rel="stylesheet">
+</head>
+<body class="article">
+<div id="cover">
+<h1>${ctx.node.getDocumentTitle()}</h1>
+<h2>${ctx.node.getDocument().getAuthor()}</h2>
+</div>
+<div id="content">
+${ctx.node.getContent()}
+</div>
+</body>`
