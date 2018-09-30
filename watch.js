44fcb818e133ea158e602db55d3a856d98b531c1
Guillaume Grossetie <ggrossetie@gmail.com>
2018-09-30 23:17:14 +0200

    Add chodikar to trigger a build

---
+++ watch.js
@@ -0,0 +1,15 @@
+const chokidar = require('chokidar');
+const child_process = require('child_process');
+
+// One-liner for current directory, ignores .dotfiles
+chokidar.watch(['examples/**/*.css', 'examples/**/*.js'], {ignored: /(^|[\/\\])\../}).on('all', (event, path) => {
+  console.log(event, path);
+  if (event === 'change') {
+    try {
+      child_process.exec('./bin/asciidoctorjs-pdf examples/cheat-sheet/maven-security-cheat-sheet.adoc --template-require ../examples/cheat-sheet/template.js');
+    } catch (e) {
+      console.log('error', e);
+    }
+  }
+});
+
