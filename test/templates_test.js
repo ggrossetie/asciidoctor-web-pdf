61a787b28b7449c26c9eb17ba008fe0c15c5d2db
Guillaume Grossetie <ggrossetie@gmail.com>
2019-09-03 15:11:49 +0200

    Do not include a title page or document title if there's no title

--- test/templates_test.js
+++ test/templates_test.js
@@ -8,32 +8,44 @@ chai.use(dirtyChai)
 const asciidoctor = require('@asciidoctor/core')()
 const templates = require('../lib/document/templates.js')
 
-describe('Default converter', function () {
-  it('should include a title page if title-page attribute is defined', function () {
+describe('Default converter', () => {
+  it('should include a title page if title-page attribute is defined', () => {
     const doc = asciidoctor.load(`= Title
 Guillaume Grossetie
 :title-page:
 
 == Section`)
     const $ = cheerio.load(templates.document(doc))
-    expect($('#cover').length).to.equal(1)
+    expect($('.title-page > h1').length).to.equal(1)
   })
 
-  it('should include a title page if doctype is book', function () {
+  it('should include a title page if doctype is book', () => {
     const doc = asciidoctor.load(`= Title
 Guillaume Grossetie
 
 == Section`, { doctype: 'book' })
     const $ = cheerio.load(templates.document(doc))
-    expect($('#cover').length).to.equal(1)
+    expect($('.title-page > h1').length).to.equal(1)
   })
 
-  it('should not include a title page', function () {
+  it('should not include a title page', () => {
     const doc = asciidoctor.load(`= Title
 Guillaume Grossetie
 
 == Section`)
     const $ = cheerio.load(templates.document(doc))
-    expect($('#cover').length).to.equal(0)
+    expect($('.title-page > h1').length).to.equal(0)
+  })
+
+  it('should not include a title page if the document title is empty', () => {
+    const doc = asciidoctor.load(`Hello world!`, { attributes: { 'title-page': '' } })
+    const $ = cheerio.load(templates.document(doc))
+    expect($('.title-page > h1').length).to.equal(0)
+  })
+
+  it('should not include a document title if the document title is empty', () => {
+    const doc = asciidoctor.load(`Hello world!`)
+    const $ = cheerio.load(templates.document(doc))
+    expect($('.title-document > h1').length).to.equal(0)
   })
 })
