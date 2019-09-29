8f1abb67fc42ff1e6a886757ad9855bdbac5f853
Guillaume Grossetie <ggrossetie@gmail.com>
2019-09-29 15:00:57 +0200

    Use Asciidoctor.convert() function in the tests

--- test/templates_test.js
+++ test/templates_test.js
@@ -6,7 +6,9 @@ const dirtyChai = require('dirty-chai')
 chai.use(dirtyChai)
 
 const asciidoctor = require('@asciidoctor/core')()
+const converter = require('../lib/converter.js')
 const templates = require('../lib/document/templates.js')
+converter.registerTemplateConverter(asciidoctor, templates)
 
 describe('Default converter', () => {
   it('should include a title page if title-page attribute is defined', () => {
@@ -15,8 +17,8 @@ Guillaume Grossetie
 :title-page:
 
 == Section`)
-    const $ = cheerio.load(templates.document(doc))
-    expect($('.title-page > h1').length).to.equal(1)
+    const $ = cheerio.load(doc.convert({ header_footer: true }))
+    expect($('.title-page > h1').text()).to.equal('Title')
   })
 
   it('should include a title page if doctype is book', () => {
@@ -24,8 +26,8 @@ Guillaume Grossetie
 Guillaume Grossetie
 
 == Section`, { doctype: 'book' })
-    const $ = cheerio.load(templates.document(doc))
-    expect($('.title-page > h1').length).to.equal(1)
+    const $ = cheerio.load(doc.convert({ header_footer: true }))
+    expect($('.title-page > h1').text()).to.equal('Title')
   })
 
   it('should not include a title page', () => {
@@ -33,19 +35,19 @@ Guillaume Grossetie
 Guillaume Grossetie
 
 == Section`)
-    const $ = cheerio.load(templates.document(doc))
+    const $ = cheerio.load(doc.convert({ header_footer: true }))
     expect($('.title-page > h1').length).to.equal(0)
   })
 
   it('should not include a title page if the document title is empty', () => {
     const doc = asciidoctor.load('Hello world!', { attributes: { 'title-page': '' } })
-    const $ = cheerio.load(templates.document(doc))
+    const $ = cheerio.load(doc.convert({ header_footer: true }))
     expect($('.title-page > h1').length).to.equal(0)
   })
 
   it('should not include a document title if the document title is empty', () => {
     const doc = asciidoctor.load('Hello world!')
-    const $ = cheerio.load(templates.document(doc))
+    const $ = cheerio.load(doc.convert({ header_footer: true }))
     expect($('.title-document > h1').length).to.equal(0)
   })
 })
