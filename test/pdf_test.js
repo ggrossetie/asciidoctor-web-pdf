712b6a51928d042ef9b07bad6517099935bc789a
Guillaume Grossetie <ggrossetie@gmail.com>
2019-12-15 16:47:45 +0100

    Adds visual tests

--- test/pdf_test.js
+++ test/pdf_test.js
@@ -77,35 +77,237 @@ describe('PDF converter', function () {
     expect(decodePDFHexStringValue(refs[7].get(PDFName.of('Title')).value)).to.equal('Section 4: Asterisk hex * and decimal *')
   })
 
-  it('should generate a PDF outline even if the TOC is absent from the output', async () => {
-    const options = { attributes: { toc: 'macro' } }
-    const pdfDoc = await convert(`${__dirname}/fixtures/sections.adoc`, `${__dirname}/output/sections-toc-absent.pdf`, options)
-    const refs = getOutlineRefs(pdfDoc)
-    expect(refs.length).to.equal(9)
-    expect(refs[0].get(PDFName.of('Dest')).encodedName).to.equal('/_section_1')
+  describe('PDF Outline', () => {
+    it('should generate a PDF outline even if the TOC is absent from the output', async () => {
+      const options = { attributes: { toc: 'macro' } }
+      const pdfDoc = await convert(`${__dirname}/fixtures/sections.adoc`, `${__dirname}/output/sections-toc-absent.pdf`, options)
+      const refs = getOutlineRefs(pdfDoc)
+      expect(refs.length).to.equal(9)
+      expect(refs[0].get(PDFName.of('Dest')).encodedName).to.equal('/_section_1')
+    })
+
+    it('should generate a PDF outline even if the TOC is not enabled', async () => {
+      const pdfDoc = await convert(`${__dirname}/fixtures/sections.adoc`, `${__dirname}/output/sections-toc-disabled.pdf`)
+      const refs = getOutlineRefs(pdfDoc)
+      expect(refs.length).to.equal(9)
+      expect(refs[0].get(PDFName.of('Dest')).encodedName).to.equal('/_section_1')
+    })
+
+    it('should honor toclevels 1 when generating a PDF outline', async () => {
+      const options = { attributes: { toclevels: 1 } }
+      const pdfDoc = await convert(`${__dirname}/fixtures/sections.adoc`, `${__dirname}/output/sections-toclevels-1.pdf`, options)
+      const refs = getOutlineRefs(pdfDoc)
+      expect(refs.length).to.equal(4)
+      expect(refs[0].get(PDFName.of('Dest')).encodedName).to.equal('/_section_1')
+    })
+
+    it('should honor toclevels 3 when generating a PDF outline', async () => {
+      const options = { attributes: { toclevels: 3 } }
+      const pdfDoc = await convert(`${__dirname}/fixtures/sections.adoc`, `${__dirname}/output/sections-toclevels-1.pdf`, options)
+      const refs = getOutlineRefs(pdfDoc)
+      expect(refs.length).to.equal(11)
+      expect(refs[0].get(PDFName.of('Dest')).encodedName).to.equal('/_section_1')
+    })
   })
 
-  it('should generate a PDF outline even if the TOC is not enabled', async () => {
-    const pdfDoc = await convert(`${__dirname}/fixtures/sections.adoc`, `${__dirname}/output/sections-toc-disabled.pdf`)
-    const refs = getOutlineRefs(pdfDoc)
-    expect(refs.length).to.equal(9)
-    expect(refs[0].get(PDFName.of('Dest')).encodedName).to.equal('/_section_1')
-  })
-
-  it('should honor toclevels 1 when generating a PDF outline', async () => {
-    const options = { attributes: { toclevels: 1 } }
-    const pdfDoc = await convert(`${__dirname}/fixtures/sections.adoc`, `${__dirname}/output/sections-toclevels-1.pdf`, options)
-    const refs = getOutlineRefs(pdfDoc)
-    expect(refs.length).to.equal(4)
-    expect(refs[0].get(PDFName.of('Dest')).encodedName).to.equal('/_section_1')
-  })
-
-  it('should honor toclevels 3 when generating a PDF outline', async () => {
-    const options = { attributes: { toclevels: 3 } }
-    const pdfDoc = await convert(`${__dirname}/fixtures/sections.adoc`, `${__dirname}/output/sections-toclevels-1.pdf`, options)
-    const refs = getOutlineRefs(pdfDoc)
-    expect(refs.length).to.equal(11)
-    expect(refs[0].get(PDFName.of('Dest')).encodedName).to.equal('/_section_1')
+  describe('Page break', () => {
+    const scenarios = [
+      {
+        doctype: 'book',
+        preamble: true,
+        section: true,
+        toc: 'preamble',
+        'title-page-attribute': false,
+        'expected-page-number': 4
+      },
+      {
+        doctype: 'book',
+        preamble: true,
+        section: true,
+        toc: 'auto',
+        'title-page-attribute': false,
+        'expected-page-number': 4
+      },
+      {
+        doctype: 'book',
+        preamble: false,
+        section: false,
+        toc: false,
+        'title-page-attribute': false,
+        'expected-page-number': 1
+      },
+      {
+        doctype: 'book',
+        preamble: false,
+        section: true,
+        toc: 'auto',
+        'title-page-attribute': false,
+        'expected-page-number': 3
+      },
+      {
+        doctype: 'book',
+        preamble: false,
+        section: true,
+        toc: false,
+        'title-page-attribute': false,
+        'expected-page-number': 2
+      },
+      {
+        doctype: 'book',
+        preamble: true,
+        section: true,
+        toc: false,
+        'title-page-attribute': false,
+        'expected-page-number': 3
+      },
+      {
+        doctype: 'article',
+        preamble: true,
+        section: true,
+        toc: 'preamble',
+        'title-page-attribute': false,
+        'expected-page-number': 1
+      },
+      {
+        doctype: 'article',
+        preamble: true,
+        section: true,
+        toc: 'auto',
+        'title-page-attribute': false,
+        'expected-page-number': 1
+      },
+      {
+        doctype: 'article',
+        preamble: false,
+        section: false,
+        toc: false,
+        'title-page-attribute': false,
+        'expected-page-number': 1
+      },
+      {
+        doctype: 'article',
+        preamble: false,
+        section: true,
+        toc: 'auto',
+        'title-page-attribute': false,
+        'expected-page-number': 1
+      },
+      {
+        doctype: 'article',
+        preamble: false,
+        section: true,
+        toc: false,
+        'title-page-attribute': false,
+        'expected-page-number': 1
+      },
+      {
+        doctype: 'article',
+        preamble: true,
+        section: true,
+        toc: false,
+        'title-page-attribute': false,
+        'expected-page-number': 1
+      },
+      {
+        doctype: 'article',
+        preamble: true,
+        section: true,
+        toc: 'preamble',
+        'title-page-attribute': true,
+        'expected-page-number': 2
+      },
+      {
+        doctype: 'article',
+        preamble: true,
+        section: true,
+        toc: 'auto',
+        'title-page-attribute': true,
+        'expected-page-number': 2
+      },
+      // disabled from now until https://gitlab.pagedmedia.org/tools/pagedjs/issues/164 is fixed.
+      // currently, paged.js will produce an empty page.
+      /*
+      {
+        doctype: 'article',
+        preamble: false,
+        section: false,
+        toc: false,
+        'title-page-attribute': true,
+        'expected-page-number': 1
+      },
+       */
+      {
+        doctype: 'article',
+        preamble: false,
+        section: true,
+        toc: 'auto',
+        'title-page-attribute': true,
+        'expected-page-number': 2
+      },
+      {
+        doctype: 'article',
+        preamble: false,
+        section: true,
+        toc: false,
+        'title-page-attribute': true,
+        'expected-page-number': 2
+      },
+      {
+        doctype: 'article',
+        preamble: true,
+        section: true,
+        toc: false,
+        'title-page-attribute': true,
+        'expected-page-number': 2
+      }
+    ]
+    for (const scenario of scenarios) {
+      const features = []
+      features.push(`type is ${scenario.doctype}`)
+      if (scenario.preamble) {
+        features.push('has preamble')
+      }
+      if (scenario.section) {
+        features.push('has section')
+      }
+      if (scenario.toc !== false) {
+        features.push(`has TOC ${scenario.toc}`)
+      }
+      if (scenario['title-page-attribute']) {
+        features.push('has :title-page: attribute')
+      }
+      let featuresDescription
+      if (features.length > 1) {
+        featuresDescription = ` ${features.slice(0, -1).join(', ')} and ${features.slice(-1)}`
+      } else {
+        featuresDescription = ` ${features.join(', ')}`
+      }
+      it(`should break pages accordingly when the document${featuresDescription}`, async () => {
+        const options = {}
+        options.attributes = {}
+        if (scenario['title-page-attribute']) {
+          options.attributes['title-page'] = ''
+        }
+        if (scenario.toc !== false) {
+          options.attributes.toc = scenario.toc
+        }
+        options.doctype = scenario.doctype
+        const outputFileName = `page-break-${scenario.doctype}-preamble_${scenario.preamble}-section_${scenario.section}-toc_${scenario.toc}-title-page-attribute_${scenario['title-page-attribute']}.pdf`
+        const outputFile = `${__dirname}/output/${outputFileName}`
+        let inputFileName
+        if (scenario.preamble && scenario.section) {
+          inputFileName = 'document-with-title-preamble-and-section.adoc'
+        } else if (scenario.section) {
+          inputFileName = 'document-with-title-and-section.adoc'
+        } else {
+          inputFileName = 'document-with-only-title.adoc'
+        }
+        const inputFile = `${__dirname}/fixtures/${inputFileName}`
+        const pdfDoc = await convert(inputFile, outputFile, options)
+        expect(pdfDoc.getPages().length).to.equal(scenario['expected-page-number'])
+        expect(outputFile).to.be.visuallyIdentical(outputFileName)
+      })
+    }
   })
 
   it('should be able to set background color of title page', async () => {
