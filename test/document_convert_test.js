ab314a631c7a2286001ab9b93b8c3cb2ca7eb869
Guillaume Grossetie <ggrossetie@gmail.com>
2021-02-12 15:35:32 +0100

    Update tests

--- test/document_convert_test.js
+++ test/document_convert_test.js
@@ -1,5 +1,6 @@
 /* global it, describe */
 const cheerio = require('cheerio')
+const ospath = require('path')
 const chai = require('chai')
 const expect = chai.expect
 const dirtyChai = require('dirty-chai')
@@ -7,6 +8,8 @@ chai.use(dirtyChai)
 
 const asciidoctor = require('@asciidoctor/core')()
 const DocumentConverter = require('../lib/document/document-converter')
+
+const fixturesPath = (...paths) => ospath.join(__dirname, 'fixtures', ...paths)
 
 describe('Document converter', () => {
   it('should override the titlePage function', () => {
@@ -29,7 +32,7 @@ Guillaume Grossetie
   describe('Docinfo', () => {
     it('should include shared (head) docinfo', () => {
       asciidoctor.ConverterFactory.register(new DocumentConverter(), ['web-pdf'])
-      const $ = cheerio.load(asciidoctor.convertFile(`${__dirname}/fixtures/simple.adoc`, {
+      const $ = cheerio.load(asciidoctor.convertFile(fixturesPath('simple.adoc'), {
         safe: 'safe',
         backend: 'web-pdf',
         header_footer: true,
@@ -41,7 +44,7 @@ Guillaume Grossetie
     })
     it('should include private (footer) docinfo', () => {
       asciidoctor.ConverterFactory.register(new DocumentConverter(), ['web-pdf'])
-      const $ = cheerio.load(asciidoctor.convertFile(`${__dirname}/fixtures/simple.adoc`, {
+      const $ = cheerio.load(asciidoctor.convertFile(fixturesPath('simple.adoc'), {
         safe: 'safe',
         backend: 'web-pdf',
         header_footer: true,
@@ -49,6 +52,17 @@ Guillaume Grossetie
         attributes: { docinfo: 'private-footer' }
       }))
       expect($('footer').text()).to.equal('This is the end.')
+    })
+    it('should include private (running) docinfo', () => {
+      asciidoctor.ConverterFactory.register(new DocumentConverter(), ['web-pdf'])
+      const $ = cheerio.load(asciidoctor.convertFile(fixturesPath('simple.adoc'), {
+        safe: 'safe',
+        backend: 'web-pdf',
+        header_footer: true,
+        to_file: false,
+        attributes: { docinfo: 'private-running' }
+      }))
+      expect($('body > .contact-us').length).to.equal(1)
     })
   })
 })
