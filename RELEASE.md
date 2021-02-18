7176a793cd453178637d9b7090807206d6b94abd
Guillaume Grossetie <ggrossetie@gmail.com>
2021-02-18 18:52:30 +0100

    Automate the release

---
+++ RELEASE.md
@@ -0,0 +1,13 @@
+# Release
+
+How to perform a release.
+
+1. Run `npm version x.y.z` at the root of the repository
+2. Push your changes with the tag: `git push origin master --tags`
+
+The release process is automated and relies on GitHub Actions.
+It will automatically publish the `asciidoctor-pdf` package on https://npmjs.com.
+In addition, it will create a release and upload artifacts on GitHub.
+
+The `NPM_TOKEN` secret is configured on GitHub.
+See the `.github/workflows/release.yml` file for details.
---
+++ tasks/publish.js
@@ -0,0 +1,28 @@
+'use strict'
+const path = require('path')
+const pacote = require('pacote') // see: http://npm.im/pacote
+const { publish: npmPublish } = require('libnpmpublish')
+
+const publish = async (directory) => {
+  if (process.env.DRY_RUN) {
+    const pkg = require(path.join(directory, 'package.json'))
+    console.log(`${pkg.name}@${pkg.version}`)
+  } else {
+    const manifest = await pacote.manifest(directory)
+    const tarData = await pacote.tarball(directory)
+    return npmPublish(manifest, tarData, { token: process.env.NPM_AUTH_TOKEN })
+  }
+}
+
+;(async () => {
+  try {
+    if (process.env.DRY_RUN) {
+      console.warn('Dry run! To publish the release, run the command again without DRY_RUN environment variable')
+    }
+    const projectRootDirectory = path.join(__dirname, '..')
+    await publish(projectRootDirectory)
+  } catch (e) {
+    console.log('Unable to publish the package', e)
+    process.exit(1)
+  }
+})()
